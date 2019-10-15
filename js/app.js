513fb662650cac345e841b1b76ac1463a54f104b
Alhanouf Alkulaib <56486855+Alhanouff@users.noreply.github.com>
2019-10-15 12:36:15 +0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -55,20 +55,40 @@ Player.prototype.update = function (dt) {
 Player.prototype.render = function () {
     ctx.drawImage(Resources.get(this.player), this.x, this.y);
 };
+// Allows the user to use the arrow keys to jump from tile to tile
 Player.prototype.handleInput = function (keyPress) {
+    
+    
+     // Enables user on left arrow key to move left on the x axis by 102
+    // Also enables user not to go off the game tiles on the left side
 if (keyPress == 'left' && this.x > 0) {
         this.x -= 102;
     };
+    
+    
+    // Enables user on right arrow key to move right on the x axis by 102
+    // Also enables user not to go off the game tiles on the right side
     if (keyPress == 'right' && this.x < 405) {
         this.x += 102;
     };
     
+    
+     // Enables user on up arrow key to move upwards on the y axis by 83
     if (keyPress == 'up' && this.y > 0) {
         this.y -= 83;
     };
+    
+    
+    
+     // Enables user on down arrow key to move downwards on the y axis by 83
+    // Also enables user not to go off the game tiles on the bottom side
     if (keyPress == 'down' && this.y < 405) {
         this.y += 83;
     };
+    
+    
+      // Once the user reaches the top of the page; the water, the user is
+    // Instantly reset to the starting position
      if (this.y < 0) {
         setTimeout(() => {
             this.x = 202;
@@ -76,14 +96,29 @@ if (keyPress == 'left' && this.x > 0) {
         }, 800);
     };
 };
+
+
+
+
+// Now instantiate your objects.
+// Place all enemy objects in an array called allEnemies
+// Place the player object in a variable called player
 var allEnemies = [];
+
+
+// Location of the 3 enemies on the y axis located on the stone road
 var enemyLocation = [63, 147, 230];
 
+
+// For each enemy located on the y axis from 0 on the x axis move at a speed of 200 
+// Until randomly regenerated in the enemy update function above
 enemyLocation.forEach(function (locationY) {
     enemy = new Enemy(0, locationY, 200);
     allEnemies.push(enemy);
 });
 
+
+// The starting location of the player is located at x=200, y=405
     var player = new Player(202, 405);
 
 
